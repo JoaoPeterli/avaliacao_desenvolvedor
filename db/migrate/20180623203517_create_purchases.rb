1c3c84512d2fc4d4787301027701067aec91b313
JoaoPeterli <janjo_pes@hotmail.com>
2018-06-23 17:55:37 -0300

    Add table purchase for dados

--- db/migrate/20180623203517_create_purchases.rb
+++ db/migrate/20180623203517_create_purchases.rb
@@ -1,7 +1,12 @@
 class CreatePurchases < ActiveRecord::Migration[5.2]
   def change
     create_table :purchases do |t|
-
+      t.string :comprador  
+      t.string :descricao 
+      t.string :preco 
+      t.string :quantidade 
+      t.string :endereco 
+      t.string :fornecedor   
       t.timestamps
     end
   end
